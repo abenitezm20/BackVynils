4eb99f8361d18ecf1305b049faef9ca9be832a7c
AlexB <a.benitezm@uniandes.edu.co>
2023-04-20 00:26:09 -0500

    Ajuste Heroku

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  //await app.listen(3000);
+  //new code
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+      console.log(`Our app is running on port ${ PORT }`);
+  });
+  //end new code
 }
 bootstrap();
